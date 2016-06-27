5b945830d3af7300e7925618b16674b6a720db3f
root <admin@example.com>
2016-06-27 21:34:43 +0200

    Test off

--- de/uschwaeke/projects/TestServlet.java
+++
@@ -1,5 +0,0 @@
-package de.uschwaeke.projects;
-
-public class TestServlet {
-
-}
---
+++ src/main/java/de/uschwaeke/projects/TestServlet.java
@@ -0,0 +1,7 @@
+package de.uschwaeke.projects;
+
+public class TestServlet {
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+}
